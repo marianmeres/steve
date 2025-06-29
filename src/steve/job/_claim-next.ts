17443863a073a8fdda383c74713a4dbca8cd0436
Marian Meres <marian@meres.sk>
2025-06-29 21:40:44 +0200

    docs

--- src/steve/job/_claim-next.ts
+++ src/steve/job/_claim-next.ts
@@ -6,20 +6,20 @@ export async function _claimNextJob(context: JobContext) {
 	const { tableJobs } = tableNames;
 
 	const result = await db.query(`
-        UPDATE ${tableJobs} 
-        SET status = '${JOB_STATUS.RUNNING}', 
-            started_at = NOW(),
-            updated_at = NOW(),
-            attempts = attempts + 1
-        WHERE id = (
-            SELECT id FROM ${tableJobs}  
-            WHERE status = '${JOB_STATUS.PENDING}' 
-            AND run_at <= NOW()
-            ORDER BY id 
-            FOR UPDATE SKIP LOCKED -- Important!
-            LIMIT 1
-        )
-        RETURNING *`);
+		UPDATE ${tableJobs} 
+		SET status = '${JOB_STATUS.RUNNING}', 
+			started_at = NOW(),
+			updated_at = NOW(),
+			attempts = attempts + 1
+		WHERE id = (
+			SELECT id FROM ${tableJobs}  
+			WHERE status = '${JOB_STATUS.PENDING}' 
+			AND run_at <= NOW()
+			ORDER BY id 
+			FOR UPDATE SKIP LOCKED -- Important!
+			LIMIT 1
+		)
+		RETURNING *`);
 
 	return result.rows[0];
 }
